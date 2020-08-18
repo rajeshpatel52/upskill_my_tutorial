63bb5606c76ba6c06e68a9562207421cfc010970
Rajesh Patel <52rajeshpatel@gmail.com>
2020-08-19 11:17:54 +1200

    Addedc code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
+	
+	# White list the following form so we can process them, if coming from
+	# a devise sign up form.
     before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,26 @@ class User < ApplicationRecord
   belongs_to :plan
 
   attr_accessor :stripe_card_token
+  # If Pro user passes validations (email, password,etc.),
+  # then call Stripe and tell Stripe to set up a subscription
+  # upon charging the customer's card.
+  # Stripe responds back with customer data.
+  # Store customer.id as the customer token and save the user.
   def save_with_subscription
 	if valid?
-		  customer.subscription = Stripe::Subscription.create(customer: customer_name, email: email, card: stripe_card_token)
-          #subscription = Stripe::Subscription.create(name: customer_name, email: email)
-          self.stripe_customer_token = customer.subscription.id
-	  
+
+          cus = Stripe::Customer.create(description: email,card: stripe_card_token)
+				self.stripe_customer_token = cus.id
+				#cus.sources.create(source: stripe_card_token)
+				#sub = Stripe::Subscription.create(customer: cus.id, plan: plan_id)
+				save!
 	    save!
 	  end
 	end
 end
 
+ #customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+ #self.stripe_customer_token = customer.id
 
-#customer = Stripe::Subscription.create(customer: 'cus_HUygIwZc2oRl6b', items: [{price: 'price_1GvyFkF9qQeqyuU6eql4MMUE'}], card: stripe_card_token)
       
- #     self.stripe_customer_token = customer.id
+ 
